c0a5f767b3618179af2a86def597262fda3d897c
Hulusi <37039143+hulusionel@users.noreply.github.com>
2019-08-16 21:58:46 +0300

    movie models added

---
+++ helper/db.js
@@ -0,0 +1,10 @@
+const mongoose=require('mongoose');
+module.exports=()=>{
+    mongoose.connect('mongodb+srv://movies:1234asdf@cluster0-sp60w.mongodb.net/test?retryWrites=true&w=majority')
+mongoose.connection.on('open',()=>{
+    console.log('MongoDB:Connected');
+});
+mongoose.connection.on('error',(err)=>{
+    console.log('MongoDB:Error',err);
+});
+}
---
+++ models/Movie.js
@@ -0,0 +1,15 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+const MovieSchema=new Schema({
+    director_id:Schema.Types.ObjectId,
+    title:{
+        type:String,
+        required:true,
+    },
+    category:String,
+    country:String,
+    year:Number,
+    imdb:Number,
+});
+module.exports=mongoose.model('movie',MovieSchema);
