24b8a9d88461b1a7ba367d0978c174f8fc4454d3
Hulusi <37039143+hulusionel@users.noreply.github.com>
2019-08-17 20:07:24 +0300

    movies [get] edit

--- routes/director.js
+++ routes/director.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const mongoose=require('mongoose');
 
 //Models
 const Director = require('../models/Director');
@@ -28,9 +29,33 @@ router.get('/', (req, res) => {
         },
         {
             $unwind: {
-                path: '$movies'
+                path: '$movies',
+                preserveNullAndEmptyArrays: true
+            }
+        },
+        {
+            $group: {
+                _id: {
+                    _id: '$_id',
+                    name: '$name',
+                    surname: '$surname',
+                    bio: '$bio'
+                },
+                movies: {
+                    $push: '$movies'
+                }
+            }
+        },
+        {
+            $project: {
+                _id: '$_id._id',
+                name: '$_id.name',
+                surname: '$_id.surname',
+                bio: '$_id.bio',
+                movies: '$movies'
             }
         }
+
     ]);
     promise.then((data) => {
         res.json({ data });
@@ -39,6 +64,80 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:director_id', (req, res) => {
+    const promise = Director.aggregate([
+        {
+            $match:{
+                '_id':mongoose.Types.ObjectId(req.params.director_id)
+            }
+        },
+        {
+            $lookup: {
+                from: 'movies',
+                localField: '_id',
+                foreignField: 'director_id',
+                as: 'movies',
+            }
+        },
+        {
+            $unwind: {
+                path: '$movies',
+                preserveNullAndEmptyArrays: true
+            }
+        },
+        {
+            $group: {
+                _id: {
+                    _id: '$_id',
+                    name: '$name',
+                    surname: '$surname',
+                    bio: '$bio'
+                },
+                movies: {
+                    $push: '$movies'
+                }
+            }
+        },
+        {
+            $project: {
+                _id: '$_id._id',
+                name: '$_id.name',
+                surname: '$_id.surname',
+                bio: '$_id.bio',
+                movies: '$movies'
+            }
+        }
+
+    ]);
+    promise.then((data) => {
+        res.json({ data });
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
+router.put('/:director_id',(req,res,next)=>{
+    const promise=Movie.findByIdAndUpdate(req.params.director_id,req.body,{new:true});
+    promise.then((data)=>{
+      if(!data)
+      next({message:'The director was not found.'})
+      res.json(data);
+    }).catch((err)=>{
+      res.json(err);
+    });
+  });
+  router.delete('/:director_id',(req,res,next)=>{
+    const promise=Director.findByIdAndDelete(req.params.director_id);
+    promise.then((data)=>{
+      if(!data)
+      next({message:'The director was not found.'})
+      res.json(data);
+    }).catch((err)=>{
+      res.json(err);
+    });
+  });
 
 
+
+  
 module.exports = router;
--- routes/movie.js
+++ routes/movie.js
@@ -4,61 +4,75 @@ const router = express.Router();
 //Models
 const Movie = require('../models/Movie');
 
-router.get('/', (req, res,next) => {
-  const promise=Movie.find({});
-  promise.then((data)=>{
-    if(!data)
-    next({message:'The movie was not fount'})
+router.get('/', (req, res, next) => {
+  const promise = Movie.aggregate([
+    {
+      $lookup: {
+        from: 'directors',
+        localField: 'director_id',
+        foreignField: '_id',
+        as: 'director',
+      }
+    },
+    {
+      $unwind: {
+        path: '$director',
+      }
+    }
+  ]);
+  promise.then((data) => {
+    if (!data)
+      next({ message: 'The movie was not fount' })
     res.json(data);
-  }).catch((err)=>{
+  }).catch((err) => {
     res.json(err);
   })
 });
 
 router.get('/top10', (req, res) => {
-  const promise=Movie.find({}).limit(10).sort({imdb:-1});
-  promise.then((data)=>{
-    if(!data)
-    next({message:'The movie was not fount'})
+  const promise = Movie.find({}).limit(10).sort({ imdb: -1 });
+  promise.then((data) => {
+    if (!data)
+      next({ message: 'The movie was not fount' })
     res.json(data);
-  }).catch((err)=>{
+  }).catch((err) => {
     res.json(err);
   })
 });
 
-router.get('/:movieId',(req,res,next)=>{
-  const promise=Movie.findById(req.params.movieId);
-  promise.then((data)=>{
-    if(!data)
-    next({message:'The movie was not found.'})
+router.get('/:movieId', (req, res, next) => {
+  const promise = Movie.findById(req.params.movieId);
+  promise.then((data) => {
+    if (!data)
+      next({ message: 'The movie was not found.' })
     res.json(data);
-  }).catch((err)=>{
+  }).catch((err) => {
     res.json(err);
   });
 });
 
-router.put('/:movieId',(req,res,next)=>{
-  const promise=Movie.findByIdAndUpdate(req.params.movieId,req.body,{new:true});
-  promise.then((data)=>{
-    if(!data)
-    next({message:'The movie was not found.'})
+router.put('/:movieId', (req, res, next) => {
+  const promise = Movie.findByIdAndUpdate(req.params.movieId, req.body, { new: true });
+  promise.then((data) => {
+    if (!data)
+      next({ message: 'The movie was not found.' })
     res.json(data);
-  }).catch((err)=>{
+  }).catch((err) => {
     res.json(err);
   });
 });
-router.delete('/:movieId',(req,res,next)=>{
-  const promise=Movie.findByIdAndDelete(req.params.movieId);
-  promise.then((data)=>{
-    if(!data)
-    next({message:'The movie was not found.'})
+router.delete('/:movieId', (req, res, next) => {
+  const promise = Movie.findByIdAndDelete(req.params.movieId);
+  promise.then((data) => {
+    if (!data)
+      next({ message: 'The movie was not found.' })
     res.json(data);
-  }).catch((err)=>{
+  }).catch((err) => {
     res.json(err);
   });
 });
 
-router.post('/', (req, res ) => {
+router.post('/', (req, res) => {
 
   const movie = new Movie(req.body);
   const promise = movie.save();
@@ -70,16 +84,16 @@ router.post('/', (req, res ) => {
   });
 });
 
-router.get('/between/:start/:end',(req,res,next)=>{
-  const {start,end}=req.params;
-  const promise=Movie.find({
-    year:{"$gte":parseInt(start),"$lte":parseInt(end)}
+router.get('/between/:start/:end', (req, res, next) => {
+  const { start, end } = req.params;
+  const promise = Movie.find({
+    year: { "$gte": parseInt(start), "$lte": parseInt(end) }
   });
-  promise.then((data)=>{
-    if(!data)
-    next({message:'The movie was not found.'})
+  promise.then((data) => {
+    if (!data)
+      next({ message: 'The movie was not found.' })
     res.json(data);
-  }).catch((err)=>{
+  }).catch((err) => {
     res.json(err);
   });
 });
