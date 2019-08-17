3feb515267532c32314994aa02ee9adbeaad09d0
Hulusi <37039143+hulusionel@users.noreply.github.com>
2019-08-17 20:52:36 +0300

    user model created

--- models/Movie.js
+++ models/Movie.js
@@ -5,12 +5,17 @@ const MovieSchema=new Schema({
     director_id:Schema.Types.ObjectId,
     title:{
         type:String,
-        required:true,
+        required:[true,'`{PATH}`,is required'],
+        maxlength:[15,'`{PATH}`({maxlength}) karakterden küçük olmalı']
     },
     category:String,
     country:String,
     year:Number,
-    imdb:Number,
+    imdb:{
+        type:Number,
+        max:10,
+        min:0
+    },
     createdAt:{
         type:Date,
         default:Date.now,
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
+const User = require('../models/User');
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/register', (req, res, next) => {
+  const { username, password } = req.body;
+  bcrypt.hash(password, 10).then((hash) => {
+    const user = new User({
+      username,
+      password:hash
+    });
+    const promise = user.save();
+    promise.then((data) => {
+      res.json(data);
+    }).catch((err) => {
+      res.json(err);
+    });
+  })
+
+});
 module.exports = router;
